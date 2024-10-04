c1d2fbee717e49db34a12439a0d057c62eba2a1e
BTavkhelidze <beqatavkhelidze4@gmail.com>
2024-10-04 22:58:51 +0400

    feature header.jsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,45 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Header.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
   return (
-    <header className="header">
-      <h1 className="heading-primary">Handmade Soaps</h1>
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="nav__item">
-            <Link className="nav__link" to="/about">
+    <header className='header'>
+      <h1 className='heading-primary'>Handmade Soaps</h1>
+      <nav className='nav'>
+        <ul className='nav__list'>
+          <li className='nav__item'>
+            <Link className='nav__link' to='/about'>
               About us
             </Link>
           </li>
-          <li className="nav__item">
-            <Link className="nav__link" to="/">
+          <li className='nav__item'>
+            <Link className='nav__link' to='/'>
               Our soaps
             </Link>
           </li>
-          <li className="nav__item">
-            <Link className="nav__link" to="/blogs">
+          <li className='nav__item'>
+            <Link className='nav__link' to='/blogs'>
               Blogs
             </Link>
           </li>
-          <li className="nav__item">
-            <Link className="nav__link" to="/contact">
+          <li className='nav__item'>
+            <Link className='nav__link' to='/contact'>
               Contact
             </Link>
           </li>
-          <li className="nav__item">
-            <Link className="nav__link" to="/assignment-3">
+          <li className='nav__item'>
+            <Link className='nav__link' to='/assignment-3'>
               Assignment 3
             </Link>
           </li>
         </ul>
       </nav>
 
-      <Link className="user" to="/profile">
-        <img className="user__img" src="/images/user.jpg" alt="user" />
-        <span className="user__name">Elene</span>
+      <Link className='user' to='/profile'>
+        <img className='user__img' src='/images/user.jpg' alt='user' />
+        <span className='user__name'>Elene</span>
       </Link>
+      <p>test beqa</p>
     </header>
   );
 }
