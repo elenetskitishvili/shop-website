bf39cc9a73f205ffaec7765019f84bed984ddc65
DataShalam <d.shalamberidze04@gmail.com>
2024-10-06 21:28:18 +0400

    Added useState in single Blog page for CSR
    - Created new file BlogDetailsClient.js where CSR is enabled
    - added error handling and simple loader
    - Using BlogDetailsClient component in /[blogId]/page.js

--- src/app/blogs/[blogId]/page.js
+++ src/app/blogs/[blogId]/page.js
@@ -1,7 +1,6 @@
-import React from "react";
-import "./Blog.css";
+import BlogDetailsClient from "./BlogDetailsClient.js";
 
-async function generateStaticParams() {
+export async function generateStaticParams() {
   const res = await fetch("https://dummyjson.com/posts");
   const data = await res.json();
   const blogs = data.posts;
@@ -10,41 +9,6 @@ async function generateStaticParams() {
   }));
 }
 
-async function BlogDetails({ params }) {
-  const { blogId } = params;
-  const res = await fetch(`https://dummyjson.com/posts/${blogId}`);
-  const blog = await res.json();
-
-  return (
-    <article className="blogPost">
-      <header className="header">
-        <h1 className="title">{blog.title}</h1>
-        <div className="meta">
-          <span className="views">{blog.views} views</span>
-          <span className="userId">Author ID: {blog.userId}</span>
-        </div>
-      </header>
-
-      <div className="content">
-        <p>{blog.body}</p>
-      </div>
-
-      <footer className="footer">
-        <div className="tags">
-          {blog.tags.map((tag, index) => (
-            <span key={index} className="tag">
-              {tag}
-            </span>
-          ))}
-        </div>
-        <div className="reactions">
-          <span className="likes">👍 {blog.reactions.likes}</span>
-          <span className="dislikes">👎 {blog.reactions.dislikes}</span>
-        </div>
-      </footer>
-    </article>
-  );
+export default function BlogDetails({ params }) {
+  return <BlogDetailsClient params={params} />;
 }
-
-export { generateStaticParams };
-export default BlogDetails;
