dc68d7ba61b43fbb1a940dff47a8bc01d77860c2
BTavkhelidze <beqatavkhelidze4@gmail.com>
2024-10-06 20:14:56 +0400

    feature products.js
    adding products page

--- src/app/components/ProductCard/ProductCard.jsx
+++ src/app/components/ProductCard/ProductCard.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import "./ProductCard.css";
+import React from 'react';
+import './ProductCard.css';
 
-function ProductCard({ productObj }) {
+function ProductCard({ productsObj }) {
+  console.log(productsObj);
   return (
-    <li className="product">
+    <li className='product'>
       <img
-        className="product__img"
-        src={productObj.image}
-        alt={productObj.name}
+        className='product__img'
+        src={productsObj.images}
+        alt={productsObj.title}
       />
-      <div className="product__details">
-        <h3 className="heading-tertiary">{productObj.name}</h3>
-        <p className="product__description">{productObj.description}</p>
-        <a href="/" className="btn btn-cta">
+      <div className='product__details'>
+        <h3 className='heading-tertiary'>{productsObj.title}</h3>
+        <p className='product__description'>{productsObj.description}</p>
+        <span>$ {productsObj.price}</span>
+        <a href='/' className='btn btn-cta'>
           Add to Cart
         </a>
       </div>
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,58 +1,41 @@
-import React from "react";
-import "./products.css";
-import ProductCard from "../components/ProductCard/ProductCard";
+'use client';
+import React, { useState, useEffect } from 'react';
 
-const productsData = [
-  {
-    name: "Rose Petal Bliss",
-    description:
-      "A gentle, rose-infused soap that nourishes and hydrates, leaving your skin soft and delicately scented.",
-    image: "images/product-1.jpg",
-  },
-  {
-    name: "Midnight Swirl",
-    description:
-      "A black and white soap with activated charcoal for a deep cleanse and shea butter for a silky, smooth feel.",
-    image: "images/product-2.jpg",
-  },
-  {
-    name: "Exotic Spice",
-    description:
-      "An aromatic set combining scents of cinnamon, clove, vanilla, and sandalwood. Perfect for those who enjoy warm, spicy fragrances.",
-    image: "images/product-3.jpg",
-  },
-  {
-    name: "Luxury Indulgence",
-    description:
-      "An opulent set with soaps made from shea butter, cocoa butter, silk protein, and argan oil. Perfect for a deeply moisturizing and pampering experience.",
-    image: "images/product-4.jpg",
-  },
-  {
-    name: "Woodland Harmony",
-    description:
-      "An earthy set with scents of cedarwood, pine, sandalwood, and patchouli. Ideal for those who enjoy the calming aroma of the forest.",
-    image: "images/product-5.jpg",
-  },
-  {
-    name: "Ocean Breeze",
-    description:
-      "A refreshing set featuring sea salt, ocean mist, algae, and kelp-infused soaps. Perfect for a beach-inspired cleanse that leaves your skin feeling revitalized.",
-    image: "images/product-6.jpg",
-  },
-];
+import './products.css';
+import ProductCard from '../components/ProductCard/ProductCard';
 
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch('https://dummyjson.com/products');
+      const data = await res.json();
+      setProducts(data.products);
+    };
+    fetchProducts();
+  }, []);
+
+  const productsData = [
+    {
+      name: 'Rose Petal Bliss',
+      description:
+        'A gentle, rose-infused soap that nourishes and hydrates, leaving your skin soft and delicately scented.',
+      image: 'images/product-1.jpg',
+    },
+  ];
+
   return (
-    <main className="main main-products">
-      <section className="section-products">
-        <p className="section-products__intro">
+    <main className='main main-products'>
+      <section className='section-products'>
+        <p className='section-products__intro'>
           Discover our collection of handmade soaps, crafted with natural
           ingredients to nourish your skin
         </p>
 
-        <ul className="products">
-          {productsData.map((product) => (
-            <ProductCard productObj={product} key={product.name} />
+        <ul className='products'>
+          {products.map((product) => (
+            <ProductCard productsObj={product} key={product.id} />
           ))}
         </ul>
       </section>
