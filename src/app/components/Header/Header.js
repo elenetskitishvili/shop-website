0180c93e2860ceb2583897969cb37983249ef4d4
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-11-03 21:51:01 +0400

    refactor(header): partially replace custom CSS with Tailwind classes

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -6,39 +6,47 @@ import { getSession } from "@auth0/nextjs-auth0";
 export default async function Header() {
   const { user } = await getSession();
   return (
-    <header className="header">
-      <h1 className="heading-primary">OmniShop</h1>
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="nav__item">
-            <Link className="nav__link" href="/about">
-              About us
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link className="nav__link" href="/products">
-              Products
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link className="nav__link" href="/blogs">
-              Blogs
-            </Link>
-          </li>
-          <li className="nav__item">
-            <Link className="nav__link" href="/contact">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <header className="w-full px-12 bg-white shadow-md text-3xl">
+      <div className="max-w-screen-xl mx-auto  flex items-center justify-between">
+        <h1 className="font-heading text-5xl tracking-tight font-medium text-emerald-500">
+          OmniShop
+        </h1>
+        <nav>
+          <ul className="flex gap-5">
+            <li className=" nav__item relative py-9 px-7 cursor-pointer transition-all duration-300 ease-in-out hover:text-emerald-500">
+              <Link className="nav__link text-inherit" href="/about">
+                About us
+              </Link>
+            </li>
+            <li className="nav__item relative py-9 px-7 cursor-pointer transition-all duration-300 ease-in-out hover:text-emerald-500">
+              <Link className="nav__link text-inherit" href="/products">
+                Products
+              </Link>
+            </li>
+            <li className="nav__item relative py-9 px-7 cursor-pointer transition-all duration-300 ease-in-out hover:text-emerald-500">
+              <Link className="nav__link text-inherit" href="/blogs">
+                Blogs
+              </Link>
+            </li>
+            <li className="nav__item relative py-9 px-7 cursor-pointer transition-all duration-300 ease-in-out hover:text-emerald-500">
+              <Link className="nav__link text-inherit" href="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="header__user">
-        <Link className="user" href="/profile">
-          <img className="user__img" src={user.picture} alt={user.name} />
-        </Link>
+        <div className="flex items-center gap-10">
+          <Link href="/profile">
+            <img
+              className="h-16 w-16 rounded-full"
+              src={user.picture}
+              alt={user.name}
+            />
+          </Link>
 
-        <a href="/api/auth/logout">Logout</a>
+          <a href="/api/auth/logout">Logout</a>
+        </div>
       </div>
     </header>
   );
