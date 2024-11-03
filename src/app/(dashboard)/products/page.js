4c1269b825220db4db6044f09a9abf23b6ab867f
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-11-03 19:53:35 +0400

    refactor(products): replace custom CSS with Tailwind classes and delete products.css and ProductCard.css

--- src/app/(dashboard)/products/page.js
+++ src/app/(dashboard)/products/page.js
@@ -1,4 +1,3 @@
-import "./products.css";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import ProductsControls from "../../components/ProductsControls/ProductsControls";
 
@@ -18,10 +17,10 @@ export default async function Products({ searchParams = {} }) {
   const products = await fetchProducts(search, sort);
 
   return (
-    <main className="main main-products">
-      <section className="section-products">
+    <main className="main">
+      <section>
         <ProductsControls searchParams={searchParams} />
-        <ul className="products">
+        <ul className="max-w-screen-xl mx-auto grid grid-cols-[repeat(auto-fit,minmax(30rem,1fr))] gap-16 mt-16 mb-24">
           {products.map((product) => (
             <ProductCard productsObj={product} key={product.id} />
           ))}
--- src/app/components/ProductCard/ProductCard.js
+++ src/app/components/ProductCard/ProductCard.js
@@ -1,19 +1,21 @@
 import Link from "next/link";
-import "./ProductCard.css";
 
 function ProductCard({ productsObj }) {
   return (
-    <li className="product">
+    <li className="flex flex-col justify-between h-full bg-white rounded-xl overflow-hidden shadow-sm transition-all duration-300 ease-in-out hover:shadow-md">
       <img
-        className="product__img"
+        className="w-full h-96 object-contain"
         src={productsObj.images[0]}
         alt={productsObj.title}
       />
-      <div className="product__details">
+      <div className="flex flex-col grow gap-7 py-10 px-14">
         <h3 className="heading-tertiary">{productsObj.title}</h3>
-        <p className="product__description">{productsObj.description}</p>
+        <p className="text-2xl mb-auto">{productsObj.description}</p>
         <span>$ {productsObj.price}</span>
-        <Link href={`/products/${productsObj.id}`} className="btn btn-cta">
+        <Link
+          href={`/products/${productsObj.id}`}
+          className="inline-block text-2xl py-5 px-10 self-start  bg-emerald-500 visited:bg-emerald-500 transition-all duration-300 ease-in-out mt-auto rounded-xl text-white hover:bg-emerald-600 active:bg-emerald-600"
+        >
           Product Details
         </Link>
       </div>
