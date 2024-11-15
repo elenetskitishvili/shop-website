f64b13439d7c2f7bb02df9f6c42d4d6ef78f093c
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-11-15 13:36:50 +0400

    fix: resolve hydration error by refactoring layout structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,16 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import "./globals.css";
 
-// import { getDictionary } from "./dictionaries";
-
 export const metadata = {
   title: "OmniShop",
   description: "Web site created with Next.js.",
 };
 
-export default async function RootLayout({ children }) {
-  // const dict = await getDictionary(lang);
+export default async function RootLayout({ children, params }) {
+  const { locale } = params || { locale: "en" };
 
   return (
-    <html>
+    <html lang={locale}>
       <body className="flex flex-col h-full font-normal text-2xl font-base text-zinc-700 bg-zinc-50 dark:text-zinc-100 dark:bg-zinc-800">
         <UserProvider>{children}</UserProvider>
       </body>
