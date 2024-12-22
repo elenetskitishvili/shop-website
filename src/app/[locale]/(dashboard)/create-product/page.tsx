defb64c7bb3a286cde6863a50d4c9bd7297c7706
nikakaladze <nikakaladze89@gmail.com>
2024-12-22 21:49:29 +0400

    feat:Add Create Product Page:

---
+++ src/app/[locale]/(dashboard)/create-product/page.tsx
@@ -0,0 +1,9 @@
+import { CreateProductForm } from "@/src/app/components/CreateProductForm";
+
+export default function CreateProduct() {
+  return (
+    <div className="flex items-center justify-center mt-44">
+      <CreateProductForm />
+    </div>
+  );
+}
