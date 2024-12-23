cd6e271008a76d21d28b9f150a0d763837454f0c
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-12-23 17:01:28 +0400

    feat: add translation support to the create-product page

--- src/app/components/CreateProductForm.tsx
+++ src/app/components/CreateProductForm.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { useTranslations } from "next-intl";
 
 import { createProduct } from "../actions/createProduct";
 import { useState } from "react";
 import SpinnerMini from "./SpinnerMini";
 
 export function CreateProductForm() {
+  const t = useTranslations("CreateProduct");
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
@@ -21,7 +23,7 @@ export function CreateProductForm() {
       setLoading(true);
 
       await createProduct(formData);
-      setSuccess("Product created successfully!");
+      setSuccess(t("successMessage"));
       form.reset();
     } catch (err) {
       setError((err as Error).message);
@@ -36,10 +38,10 @@ export function CreateProductForm() {
       onSubmit={handleSubmit}
       className="rounded-lg p-4 bg-gray-100 flex flex-col gap-6 min-w-[400px]"
     >
-      <label htmlFor="name">Name</label>
+      <label htmlFor="name">{t("name")}</label>
       <input type="text" id="name" name="name" required />
 
-      <label htmlFor="price">Price</label>
+      <label htmlFor="price">{t("price")}</label>
       <input
         type="number"
         id="price"
@@ -49,29 +51,29 @@ export function CreateProductForm() {
         required
       />
 
-      <label htmlFor="description">Description</label>
+      <label htmlFor="description">{t("description")}</label>
       <textarea
         id="description"
         name="description"
         rows={4}
         required
-        placeholder="Enter description of the product..."
+        placeholder={t("descriptionPlaceholder")}
         className="py-2 px-3"
       />
 
-      <label htmlFor="image">Image</label>
+      <label htmlFor="image">{t("image")}</label>
       <input type="file" id="image" name="image" accept="image/*" required />
 
       {loading && (
         <div className="flex items-center justify-center gap-5">
           <SpinnerMini />
-          <p className=" text-2xl text-center">Creating Product...</p>
+          <p className=" text-2xl text-center">{t("creatingProduct")}</p>
         </div>
       )}
 
       {error && (
         <p className="text-orange-700 text-2xl text-center">
-          Failed to create a new product.
+          {t("failMessage")}
         </p>
       )}
 
@@ -81,18 +83,18 @@ export function CreateProductForm() {
 
       <button
         type="submit"
-        className={`bg-black text-white py-4 ${
+        className={`rounded-sm bg-black text-white py-4 hover:bg-gray-800 transition-all ${
           loading ? "opacity-50 cursor-not-allowed" : "opacity-100"
         }`}
         disabled={loading}
       >
         {loading
-          ? "Creating..."
+          ? t("creating")
           : success
-          ? "Create Another"
+          ? t("createAnother")
           : error
-          ? "Retry"
-          : "Create"}
+          ? t("retry")
+          : t("create")}
       </button>
     </form>
   );
