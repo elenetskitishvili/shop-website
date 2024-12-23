603120e1a458e6c1a063d659b06c0f66260bb86d
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-12-23 12:47:47 +0400

    feat: add loading spinner and improve form submission feedback

--- src/app/components/CreateProductForm.tsx
+++ src/app/components/CreateProductForm.tsx
@@ -2,26 +2,40 @@
 
 import { createProduct } from "../actions/createProduct";
 import { useState } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 export function CreateProductForm() {
   const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = async (formData: FormData) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
     try {
+      setError(null);
+      setSuccess(null);
+      setLoading(true);
+
       await createProduct(formData);
-      alert("Product created successfully!");
+      setSuccess("Product created successfully!");
+      form.reset();
     } catch (err) {
       setError((err as Error).message);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <form
-      action={handleSubmit}
+      // action={handleSubmit}
+      onSubmit={handleSubmit}
       className="rounded-lg p-4 bg-gray-100 flex flex-col gap-6 min-w-[400px]"
     >
-      {error && <p className="text-red-500">{error}</p>}
-
       <label htmlFor="name">Name</label>
       <input type="text" id="name" name="name" required />
 
@@ -35,8 +49,37 @@ export function CreateProductForm() {
         required
       />
 
-      <button type="submit" className="bg-black text-white py-4">
-        Create
+      {loading && (
+        <div className="flex items-center justify-center gap-5">
+          <SpinnerMini />
+          <p className=" text-2xl text-center">Creating Product...</p>
+        </div>
+      )}
+
+      {error && (
+        <p className="text-orange-700 text-2xl text-center">
+          Failed to create a new product.
+        </p>
+      )}
+
+      {success && (
+        <p className="text-green-700 text-2xl text-center">{success}</p>
+      )}
+
+      <button
+        type="submit"
+        className={`bg-black text-white py-4 ${
+          loading ? "opacity-50 cursor-not-allowed" : "opacity-100"
+        }`}
+        disabled={loading}
+      >
+        {loading
+          ? "Creating..."
+          : success
+          ? "Create Another"
+          : error
+          ? "Retry"
+          : "Create"}
       </button>
     </form>
   );
