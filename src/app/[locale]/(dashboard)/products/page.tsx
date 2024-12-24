24c80c5167dc4e918c83b051514b2297afb0faab
Mebo <samkharadzemerab@gmail.com>
2024-12-24 19:53:55 +0400

    fix: fixed products position on products page

--- src/app/[locale]/(dashboard)/products/page.tsx
+++ src/app/[locale]/(dashboard)/products/page.tsx
@@ -22,16 +22,14 @@ export default async function Products({
   const { locale } = await params;
   const products: Product[] = await fetchProducts();
 
-  console.log(products)
-
   return (
-    <section className="relative max-w-screen-xl flex flex-col justify-center">
-      <button className="absolute right-0 top-0 w-[200px] shadow-lg flex p-2 my-6 text-3xl text-emerald-500 hover:text-emerald-700">
+    <section className="relative max-w-screen-xl mx-auto flex flex-col justify-center">
+      <button className="absolute right-0 top-0 w-[200px] shadow-lg flex items-center justify-center p-2 my-6 text-3xl text-emerald-500 hover:text-emerald-700">
         <Link href={`/${locale}/create-product`} className="w-full">
           Create A Product
         </Link>
-      </button>{" "}
-      <ul className="mx-auto grid grid-cols-[repeat(auto-fit,minmax(22.5rem,1fr))] gap-20 mt-28 mb-24">
+      </button>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-28 mb-24">
         {products.map((product) => (
           <ProductCard product={product} key={product.id} locale={locale} />
         ))}
