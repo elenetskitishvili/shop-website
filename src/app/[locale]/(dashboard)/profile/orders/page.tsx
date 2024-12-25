900d2fc23300d5f859dfa633b307535b1a613153
sangambash7 <san.gambashidze@gmail.com>
2024-12-25 14:49:34 +0200

    style: created order table design

---
+++ src/app/[locale]/(dashboard)/profile/orders/page.tsx
@@ -0,0 +1,37 @@
+import { createClient } from "@/src/utils/supabase/server";
+
+async function Orders() {
+  return (
+    <section className="h-full bg-gradient-to-tr py-12 from-emerald-500 to-emerald-200 dark:from-zinc-800 dark:to-zinc-800">
+      <div className="flex justify-center">
+        <div className="bg-white dark:bg-zinc-900 w-full md:w-[50%] flex flex-col gap-4 pt-8 justify-center text-center rounded-md">
+          <h2 className="text-zinc-500 dark:text-zinc-50 text-4xl">
+            Order History
+          </h2>
+          <table className="table-auto">
+            {/* Table Heading Row */}
+            <thead>
+              <tr>
+                <th>Product ID</th>
+                <th>Order Created</th>
+                <th>Price</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            {/* Table Content Rows */}
+            <tbody>
+              <tr className="bg-[#f2f2f2]">
+                <td>product_id</td>
+                <td>created_at</td>
+                <td>order.price</td>
+                <td>description</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Orders;
