77d56f742b141eb0552d4ec9e5feb88e1fc60325
elenetskitishvili <elene.tskitishvili504@gmail.com>
2025-01-16 21:03:49 +0400

    test(e2e): add Cypress test for delete product functionality

--- cypress/e2e/createAndDeleteProduct.cy.ts
+++ cypress/e2e/createAndDeleteProduct.cy.ts
@@ -1,4 +1,6 @@
-describe("Create Product", () => {
+describe("Create and Delete Product", () => {
+  const productName = "Test Product";
+
   beforeEach(() => {
     cy.fixture("user.json").then((userData) => {
       cy.login(userData.validEmail, userData.validPassword);
@@ -9,8 +11,7 @@ describe("Create Product", () => {
   it("creates product successfully", () => {
     cy.get('[data-cy="create-product-link"]').click();
 
-    // fill in the form
-    cy.get('[data-cy="product-name-input"]').type("Test Product");
+    cy.get('[data-cy="product-name-input"]').type(productName);
     cy.get('[data-cy="product-price-input"]').type("45.99");
     cy.get('[data-cy="product-description-input"]').type(
       "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
@@ -21,6 +22,21 @@ describe("Create Product", () => {
     );
 
     cy.get('[data-cy="create-product-button"]').click();
+    cy.wait(3000);
     cy.get('[data-cy="product-creation-success-message"]').should("be.visible");
+  });
+
+  it("deletes product successfully", () => {
+    cy.get('[data-cy="product-card"]').should("be.visible");
+    cy.get('[data-cy="product-card"]')
+      .contains(productName)
+      .parents('[data-cy="product-card"]')
+      .within(() => {
+        cy.get('[data-cy="delete-product-button"]').click();
+      });
+
+    cy.get('[data-cy="product-card"]')
+      .contains(productName)
+      .should("not.exist");
   });
 });
